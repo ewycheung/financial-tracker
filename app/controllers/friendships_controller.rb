89e7e5281ff1b77f7381902716775208919c44b7
ewycheung <cheung_eddie@rogers.com>
2021-04-09 11:35:41 -0400

    Add search friend and follow frient feature

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,18 @@
 class FriendshipsController < ApplicationController
-  def destroy
-    friend = User.find(params[:id])
-    friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
+  def create    
+    current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"      
+    end
+    redirect_to my_friends_path
+  end
+
+  def destroy    
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
-    flash[:notice] = "Friend #{friend.full_name} was successfully unfollowed."
+    flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
 end
